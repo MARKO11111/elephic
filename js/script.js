b13050e7352135578994a86b4f0f9954f9caa6e3
Marko Bilic <bilmarko@gmail.com>
2023-04-24 13:58:53 +0200

    inital commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Slider:
+
+const productContainers = [...document.querySelectorAll('.product_container')];
+const nxtBtn = [...document.querySelectorAll('.nxt_btn')];
+const preBtn = [...document.querySelectorAll('.pre_btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+// Accordion:
+
+var acc = document.getElementsByClassName("accordion__button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
